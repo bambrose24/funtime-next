7b4a58baf1b6c0758c5626e0b7e22fb826d29897
Bob Ambrose <bambrose24@gmail.com>
2022-08-01 09:35:09 -0400

    update theme to not use system color mode

--- src/util/theme.ts
+++ src/util/theme.ts
@@ -2,6 +2,7 @@ import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 const themeConfig: ThemeConfig = {
   initialColorMode: "light",
+  useSystemColorMode: false,
 };
 const theme = extendTheme({
   config: themeConfig,

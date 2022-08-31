8bffe960f089425729c2c8949234b56c6440ad9e
Bob Ambrose <bambrose24@gmail.com>
2022-08-31 16:47:37 -0400

    make week page with temp text

--- pages/week/index.tsx
+++ pages/week/index.tsx
@@ -2,18 +2,16 @@ import { Flex } from "@chakra-ui/react";
 import { Typography } from "@src/components/Typography";
 import FuntimePage from "@src/FuntimePage";
 
-const SeasonPage: React.FC = () => {
+const WeekPage: React.FC = () => {
   return (
     <FuntimePage>
       <Flex w="100%" align="center">
         <Flex w="100%" maxWidth="1000px" align="center" justify="center">
-          <Typography.H2>
-            All weekly picks will be available here once the first week starts.
-          </Typography.H2>
+          <Typography.H2>Coming soon!</Typography.H2>
         </Flex>
       </Flex>
     </FuntimePage>
   );
 };
 
-export default SeasonPage;
+export default WeekPage;

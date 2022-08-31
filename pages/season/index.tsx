dd4962bb2485d6983f3c4e3e2a16196cd473e8bf
Bob Ambrose <bambrose24@gmail.com>
2022-08-31 16:49:04 -0400

    oops add the right message to the week page

--- pages/season/index.tsx
+++ pages/season/index.tsx
@@ -7,9 +7,7 @@ const SeasonPage: React.FC = () => {
     <FuntimePage>
       <Flex w="100%" align="center">
         <Flex w="100%" maxWidth="1000px" align="center" justify="center">
-          <Typography.H2>
-            All weekly picks will be available here once the first week starts.
-          </Typography.H2>
+          <Typography.H2>Coming soon!</Typography.H2>
         </Flex>
       </Flex>
     </FuntimePage>

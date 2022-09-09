5f67d999bf640e0bf3d596529a0a2538998336e8
Bob Ambrose <bambrose24@gmail.com>
2022-09-08 22:12:25 -0400

    remove the week fallback

--- pages/week/index.tsx
+++ pages/week/index.tsx
@@ -1,25 +1,7 @@
 import FuntimePage from "@src/FuntimePage";
-import { Typography } from "@src/components/Typography";
-import { Flex } from "@chakra-ui/react";
 import WeekContent from "@src/components/week/WeekContent";
-import { env } from "@src/util/config";
 
 const WeekPage: React.FC = () => {
-  if (env === "production") {
-    return (
-      <FuntimePage>
-        <Flex w="100%" align="center">
-          <Flex w="100%" maxWidth="1000px" align="center" justify="center">
-            <Typography.H2>
-              All weekly picks will be available here once the first week
-              starts.
-            </Typography.H2>
-          </Flex>
-        </Flex>
-      </FuntimePage>
-    );
-  }
-
   return (
     <FuntimePage>
       <WeekContent />

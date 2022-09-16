2d29d60d2597bca59a4393e341b64a9b4e1d65ab
Benjamin Tully <benjamin.tully@politemail.com>
2022-09-16 14:35:06 -0400

    tooltip on user tag

--- src/components/profile/UserTag.tsx
+++ src/components/profile/UserTag.tsx
@@ -1,4 +1,4 @@
-import {Button, HStack} from "@chakra-ui/react";
+import {Button, HStack, Tooltip} from "@chakra-ui/react";
 import ProfilePicture from "./ProfilePicture";
 import { Typography } from "../Typography";
 import React from "react";
@@ -9,12 +9,14 @@ const UserTag = (props: { user_id: number, username: string, mobileView?: boolea
 
   return(
     <a href={profile_url}>
+      <Tooltip label={props.username} aria-label='A tooltip'>
       <Button size="sm" borderRadius="25px" border="2px solid" borderColor="gray.200" px={2} zIndex="20" bg="white">
-        <HStack width={props.mobileView ? "75px" : "initial"}> 
+      <HStack width={props.mobileView ? "75px" : "initial"}>
           <ProfilePicture id={props.user_id} username={props.username} size="xs"/>
           <Typography.Subtitle1 overflow={props.mobileView ? "hidden" : "initial"} textOverflow={props.mobileView ? "ellipsis" : "initial"} pl={0} color="black">{props.username}</Typography.Subtitle1>
         </HStack>
       </Button>
+      </Tooltip>
     </a>
   );
 }

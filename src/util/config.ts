e19c9d035a06dc0b38b7d4c39688be3cfb683900
Bob Ambrose <bambrose24@gmail.com>
2023-03-05 09:46:17 -0500

    dummy commit 2

--- src/util/config.ts
+++ src/util/config.ts
@@ -7,8 +7,6 @@ export type Config = {
   graphqlEndpoint: string;
 };
 
-console.log("hi again");
-
 const configMap: Record<Env, Config> = {
   development: {
     graphqlEndpoint: "http://localhost:3001/graphql",

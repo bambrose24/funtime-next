3db6ced9cca3172431780548a08a8366d6598148
Bob Ambrose <bambrose24@gmail.com>
2023-05-21 12:21:01 -0400

    dummy 1

--- src/components/home/league-cards/LeagueCardFooter.tsx
+++ src/components/home/league-cards/LeagueCardFooter.tsx
@@ -58,6 +58,7 @@ export function LeagueCardFooter({member}: LeagueCardFooterProps) {
             {member.role === MemberRole.Admin && <MenuItem>Manage League (admin only)</MenuItem>}
             <MenuItem>Standings</MenuItem>
             <MenuItem>Make Picks</MenuItem>
+            <MenuItem>Make Picks (remove this)</MenuItem>
           </MenuList>
         </Menu>
       </Flex>

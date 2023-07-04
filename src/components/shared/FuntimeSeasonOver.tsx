36c85f966874e63ea658e938ce1bee9c7c131b96
Bob Ambrose <bambrose24@gmail.com>
2023-07-03 21:09:26 -0400

    ignore lints during builds for now

--- src/components/shared/FuntimeSeasonOver.tsx
+++ src/components/shared/FuntimeSeasonOver.tsx
@@ -1,4 +1,5 @@
 import {Flex} from '@chakra-ui/react';
+import Link from 'next/link';
 import {Typography} from '../Typography';
 
 export const FuntimeSeasonOver = () => {
@@ -7,13 +8,13 @@ export const FuntimeSeasonOver = () => {
       <Typography.H3>The season is over! Thanks for playing.</Typography.H3>
       <Typography.H5>
         Stay tuned for next year! You can still check out the{' '}
-        <a href="/season">
+        <Link href="/season">
           <u>standings</u>
-        </a>{' '}
+        </Link>{' '}
         and{' '}
-        <a href="/superbowl">
+        <Link href="/superbowl">
           <u>superbowl</u>
-        </a>{' '}
+        </Link>{' '}
         pages.
       </Typography.H5>
     </Flex>

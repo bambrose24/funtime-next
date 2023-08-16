4cf26fb334f91e85eabc39937db675865eacac40
Bob Ambrose <bambrose24@gmail.com>
2023-08-15 22:21:25 -0400

    no pre build for season page

--- pages/standings/[leagueId].tsx
+++ pages/standings/[leagueId].tsx
@@ -1,22 +1,9 @@
-import {gql} from '@apollo/client';
 import {StandingsPage} from '@src/components/season/StandingsPage';
 import {FuntimeError} from '@src/components/shared/FuntimeError';
 import FuntimePage from '@src/FuntimePage';
-import {AllLeaguesDocument, AllLeaguesQuery} from '@src/generated/graphql';
-import {getApolloClient} from '@src/graphql';
-import {GetStaticPaths, GetStaticProps} from 'next';
 import {useRouter} from 'next/router';
 
-const AllLeaguesQueryDoc = gql`
-  query AllLeagues {
-    leagues {
-      id
-      league_id
-    }
-  }
-`;
-
-function SeasonPage() {
+export default function SeasonPage() {
   const router = useRouter();
   const leagueIdParam = router.query.leagueId;
 
@@ -26,26 +13,3 @@ function SeasonPage() {
     <FuntimePage>{leagueId ? <StandingsPage leagueId={leagueId} /> : <FuntimeError />}</FuntimePage>
   );
 }
-
-export const getStaticProps: GetStaticProps = () => {
-  return {
-    props: {},
-    revalidate: 60 * 5,
-  };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const client = getApolloClient();
-  const {data} = await client.query<AllLeaguesQuery>({
-    query: AllLeaguesDocument,
-  });
-  return {
-    fallback: 'blocking',
-    paths:
-      data?.leagues?.map(league => {
-        return `/season/${league.league_id.toString()}`;
-      }) ?? [],
-  };
-};
-
-export default SeasonPage;

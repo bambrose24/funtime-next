54c33f51170a245a9f8e04e80546535649d50e22
Bob Ambrose <bambrose24@gmail.com>
2023-08-24 15:58:13 -0400

    reset password page

--- src/components/login/Signin.tsx
+++ src/components/login/Signin.tsx
@@ -11,6 +11,7 @@ import {
 import {useFullURL} from '@src/hooks/useFullURL';
 import {useSupabaseClient} from '@supabase/auth-helpers-react';
 import {Formik} from 'formik';
+import {useRouter} from 'next/router';
 import {useState} from 'react';
 import * as Yup from 'yup';
 import {Typography} from '../Typography';
@@ -31,6 +32,7 @@ const validationSchema = Yup.object({
 export function Signin() {
   const supabase = useSupabaseClient();
   const fullURL = useFullURL();
+  const router = useRouter();
   const [resetPasswordEmailSubmitted, setResetPasswordEmailSubmitted] = useState('');
   const toaster = useToast();
   return (
@@ -102,25 +104,28 @@ export function Signin() {
             >
               Log In
             </Button>
-            {/* https://supabase.com/docs/guides/auth/auth-password-reset */}
-            {/* <Button
+            <Button
               variant="outline"
               onClick={() => {
                 const email = formik.values.email;
-                supabase.auth.resetPasswordForEmail(email, {redirectTo: fullURL}).then(res => {
-                  console.log({resetRes: res});
-                  toaster({
-                    variant: 'solid',
-                    position: 'top',
-                    description: `A password reset email was sent to ${email}. Please check your email to reset your password.`,
+                supabase.auth
+                  .resetPasswordForEmail(email, {
+                    redirectTo: `https://www.play-funtime.com/reset-password?redirectTo=${router.asPath}`,
+                  })
+                  .then(res => {
+                    console.log({resetRes: res});
+                    toaster({
+                      variant: 'solid',
+                      position: 'top',
+                      description: `A password reset email was sent to ${email}. Please check your email to reset your password.`,
+                    });
+                    setResetPasswordEmailSubmitted(email);
                   });
-                  setResetPasswordEmailSubmitted(email);
-                });
               }}
               disabled={!formik.values.email || formik.values.email === resetPasswordEmailSubmitted}
             >
               Forgot Password
-            </Button> */}
+            </Button>
           </Flex>
         );
       }}

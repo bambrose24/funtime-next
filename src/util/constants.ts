b5228458aeddd19383ab91241389240f228f928d
Bob Ambrose <bambrose24@gmail.com>
2023-09-10 22:42:03 -0400

    allow simulations too

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,1 +1,8 @@
+import {LatePolicy} from '@src/generated/graphql';
+
 export const SECONDS_IN_DAY = 60 * 60 * 24;
+
+export const showUnstartedLatePolicies: LatePolicy[] = [
+  LatePolicy.AllowLateAndLockAfterStart,
+  LatePolicy.CloseAtFirstGameStart,
+];

c279941bf200d00f0b9763d6d4fe13c11c31eaec
Bob Ambrose <bambrose24@gmail.com>
2024-01-02 09:54:22 -0500

    update poll intervals

--- src/modules/season/WeeklyWinners.tsx
+++ src/modules/season/WeeklyWinners.tsx
@@ -25,6 +25,7 @@ import {Typography} from '../Typography';
 export function WeeklyWinners({leagueId}: {leagueId: number}) {
   const {data, error} = useWinnersQuery({
     variables: {league_id: leagueId},
+    pollInterval: 1000 * 60 * 3, // every 5 minutes
   });
   if (error) {
     return <Typography.H3>There was an error loading winners for the year.</Typography.H3>;

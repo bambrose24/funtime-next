0b1f9d22481b375f0b8aca287b21bacad60daf37
Bob Ambrose <bambrose24@gmail.com>
2024-01-06 16:46:02 -0500

    season over page with correct link

--- src/modules/shared/FuntimeSeasonOver.tsx
+++ src/modules/shared/FuntimeSeasonOver.tsx
@@ -1,21 +1,17 @@
-import {Flex} from '@chakra-ui/react';
-import Link from 'next/link';
-import {Typography} from '../Typography';
+import { Flex } from "@chakra-ui/react";
+import Link from "next/link";
+import { Typography } from "../Typography";
 
-export const FuntimeSeasonOver = () => {
+export const FuntimeSeasonOver = ({ leagueId }: { leagueId: number }) => {
   return (
     <Flex direction="column" mx="24px" justify="center" textAlign="center">
       <Typography.H3>The season is over! Thanks for playing.</Typography.H3>
       <Typography.H5>
-        Stay tuned for next year! You can still check out the{' '}
-        <Link href="/season">
+        Stay tuned for next year! You can still check out the{" "}
+        <Link href={`league/${leagueId}/season`}>
           <u>standings</u>
-        </Link>{' '}
-        and{' '}
-        <Link href="/superbowl">
-          <u>superbowl</u>
-        </Link>{' '}
-        pages.
+        </Link>{" "}
+        and see Super Bowl picks there.
       </Typography.H5>
     </Flex>
   );

fbeccc2217d6366f26fa13b986c146dca66b719a
Bob Ambrose <bambrose24@gmail.com>
2024-03-09 14:06:44 -0500

    init posthog

--- src/analytics/AnalyticsProvider.tsx
+++ src/analytics/AnalyticsProvider.tsx
@@ -1,7 +1,23 @@
 import {useUser} from '@supabase/auth-helpers-react';
 // import {Analytics} from '@vercel/analytics/react';
+import posthog from 'posthog-js';
+import {PostHogProvider} from 'posthog-js/react';
 import LogRocket from 'logrocket';
 import {ReactNode, useEffect} from 'react';
+import {env} from '@src/util/config';
+
+// init posthog
+if (typeof window !== 'undefined') {
+  // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? '', {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    loaded: posthog => {
+      if (env === 'development') {
+        posthog.debug(); // debug mode in development
+      }
+    },
+  });
+}
 
 export function AnalyticsProvider({children}: {children: ReactNode}) {
   const user = useUser();
@@ -13,10 +29,10 @@ export function AnalyticsProvider({children}: {children: ReactNode}) {
   }, [user]);
 
   return (
-    <>
+    <PostHogProvider client={posthog}>
       {children}
       {/* Removing because I hit the quota so fast and want other sites on my project to use analytics */}
       {/* <Analytics /> */}
-    </>
+    </PostHogProvider>
   );
 }
